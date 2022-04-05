ac8c2719d3c181796a50f822b416168e31c08102
zyzalfors <55386215+zyzalfors@users.noreply.github.com>
2022-04-05 22:24:19 +0200

    Add files via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE Pantry
+
+USE Pantry
+
+CREATE TABLE Pantries
+(
+ PantryID int IDENTITY (1,1) PRIMARY KEY,
+ PantryDesc nvarchar(50)
+);
+
+CREATE TABLE Packages
+(
+ PackageID int IDENTITY (1,1) PRIMARY KEY,
+ PackageDesc nvarchar(50),
+ PackageInteg int NOT NULL,
+ PantryID int NOT NULL FOREIGN KEY REFERENCES Pantries(PantryID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Products
+( 
+ ProductID int IDENTITY (1,1) PRIMARY KEY,
+ ProductDesc nvarchar(50),
+ PackageID int NOT NULL FOREIGN KEY REFERENCES Packages(PackageID) ON DELETE CASCADE ON UPDATE CASCADE
+);
